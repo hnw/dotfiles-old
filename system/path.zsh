51e949a46a4a44b3f35220019d893461530476ca
Yoshio HANAWA <y@hnw.jp>
2015-03-23 19:54:32 +0900

    Androidの設定を別ディレクトリにした / 不要なファイルを削除

--- system/path.zsh
+++ system/path.zsh
@@ -1,11 +1,3 @@
-export PATH="$HOME/bin:$HOME/.phpenv/bin:/Users/hanawa-y/.phpenv/shims:$HOME/.rvm/bin:/usr/local/bin:/usr/local/share/python:/usr/local/sbin:/usr/bin:/bin:/usr/sbin:/sbin:$ZSH/bin:$PATH"
+export PATH="$HOME/bin:$HOME/.phpenv/bin:/Users/hanawa-y/.phpenv/shims:$HOME/.rvm/bin:$HOME/Library/Haskell/bin:/usr/local/bin:/usr/local/share/python:/usr/local/sbin:/usr/bin:/bin:/usr/sbin:/sbin:$ZSH/bin:$PATH"
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
-
-export ANDROID_SDK_PATH="$HOME/Development/adt-bundle-mac-x86_64-20130717/sdk"
-export ANDROID_SDK_ROOT=$ANDROID_SDK_PATH
-export PATH=${PATH}:$ANDROID_SDK_PATH/platform-tools:$ANDROID_SDK_PATH/tools
-
-export ANDROID_NDK_PATH="$HOME/Development/android-ndk-r9"
-export ANDROID_NDK_ROOT=$ANDROID_NDK_PATH
-export PATH=$PATH:$ANDROID_NDK_PATH
